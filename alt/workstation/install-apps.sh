cbe371a5659fc6b86edfb80de3fc9d969c63eb03
eppv <evgeniypalych@gmail.com>
2025-07-17 15:00:40 +1000

    Update install-apps.sh

--- alt/workstation/install-apps.sh
+++ alt/workstation/install-apps.sh
@@ -10,6 +10,9 @@ apt-get update &&
 echo "Installing basic apps with APT..."
 apt-get install -y git alacritty telegram-desktop neofetch btop &&
 
+# basic apps with Flatpak
+flatpak install -y flathub md.obsidian.Obsidian
+
 # HOME override!
 HOME="/home/$USER_NAME"
 
@@ -18,3 +21,13 @@ sudo -E -u $USER_NAME bash -c 'echo "Installing Zed...to $HOME"'
 sudo -E -u $USER_NAME bash -c 'curl -f https://zed.dev/install.sh | sh'
 sudo -E -u $USER_NAME bash -c 'echo "export PATH=$HOME/.local/bin:$PATH" >> $HOME/.bashrc'
 sudo -E -u $USER_NAME bash -c 'echo "export PATH=$HOME/.local/bin:$PATH" >> $HOME/.zshrc'
+
+
+# User apps configuration
+
+## zsh
+usermod root -s /bin/zsh
+usermod $USER_NAME -s /bin/zsh
+ln -s /home/$USER_NAME/.zshrc /root/.zshrc
+
+echo "Please reboot your system for changes to take effect."
