444ba5f0bc8dc0249cc373498e9ee14c6a64ea4a
eppv <evgeniypalych@gmail.com>
2025-07-19 01:32:25 +1000

    Update install-apps.sh

--- alt/workstation/install-apps.sh
+++ alt/workstation/install-apps.sh
@@ -3,31 +3,39 @@
 HOSTNAME=$(hostname)
 USER_NAME=$(id -nu 1000)
 
-# Install applications
 apt-get update &&
 
-# basic apps with APT
+# basic apps
+## with APT
 echo "Installing basic apps with APT..."
 apt-get install -y git alacritty telegram-desktop neofetch btop &&
 
-# basic apps with Flatpak
+## with Flatpak
 flatpak install -y flathub md.obsidian.Obsidian
 
 # HOME override!
 HOME="/home/$USER_NAME"
 
-# zed
+## zed
 echo "Installing Zed...to $HOME"
 curl -f https://zed.dev/install.sh | sh
 echo "export PATH=$HOME/.local/bin:$PATH" >> $HOME/.bashrc
 echo "export PATH=$HOME/.local/bin:$PATH" >> $HOME/.zshrc
 
+## syncthing
+## TODO: download syncthing
+sudo tar -C '/opt' -xvf Downloads/syncthing*
+mkdir -p .config/systemd/user
+mv Downloads/syncthing.service ~/.config/systemd/user/syncthing.service
 
-# User apps configuration
+# work apps
 
-## zsh
-usermod root -s /bin/zsh
-usermod $USER_NAME -s /bin/zsh
-ln -s /home/$USER_NAME/.zshrc /root/.zshrc
+# with APT
+apt-get install -y openconnect gnome-connections
 
-echo "Please reboot your system for changes to take effect."
+# dev apps
+## uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+## rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
